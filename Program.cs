89f45b0ed10286276217ba71646491867ddbe23c
chipcasla <alejandroforesi15@gmail.com>
2025-01-08 19:58:46 -0300

    Fix conflicts

--- Program.cs
+++ Program.cs
@@ -74,12 +74,8 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-<<<<<<< HEAD
     var dbContext = services.GetRequiredService<ApplicationDbContext>();
-    SeedDatabase(dbContext);
-=======
     await RoleInitializer.CreateRoles(services);
->>>>>>> alvaro
 }
 
 
@@ -101,40 +97,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-void SeedDatabase(ApplicationDbContext context)
-{
-    if (!context.Categories.Any())
-    {
-        context.Categories.AddRange(new[]
-        {
-            new Category { Id = 1, Name = "Alimentos" },
-            new Category { Id = 2, Name = "Ropa" },
-            new Category { Id = 3, Name = "Calzado" },
-            new Category { Id = 4, Name = "Juguetes" },
-            new Category { Id = 5, Name = "Material escolar" }
-        });
-    }
-
-    if (!context.Centers.Any())
-    {
-        context.Centers.AddRange(new[]
-        {
-            new Center { Id = 1, Name = "Centro Caritas 1", Location = "Calle de la Caridad, 1", Manager = "Juan Pérez", CapacityLimit = 100, Phone = "123456789" },
-            new Center { Id = 2, Name = "Centro Caritas 2", Location = "Calle de Dios, 2", Manager = "María López", CapacityLimit = 150, Phone = "987654321" }
-        });
-    }
-
-    if (!context.DonationRequests.Any())
-    {
-        context.DonationRequests.AddRange(new[]
-        {
-            new DonationRequest { Id = 1, AssignedCenterId = 1, ShipmentDate = DateTime.Now, ReceptionDate = DateTime.Now, Status = "Recibido" },
-            new DonationRequest { Id = 2, AssignedCenterId = 2, ShipmentDate = DateTime.Now, ReceptionDate = DateTime.Now, Status = "Recibido" }
-        });
-    }
-
-    context.SaveChanges();
-}
-
