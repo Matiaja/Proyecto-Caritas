fcb32c998285050e779beca88a65621c09786e93
Matiaja <matias.petrich@gmail.com>
2025-04-19 22:24:53 -0300

    Refactor: implement OnInit lifecycle hook to manage user authentication and navbar visibility

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BreadcrumbComponent } from './shared/components/breadcrumbs/breadcrumbs.component';
+import { GlobalStateService } from './services/global/global-state.service';
 
 @Component({
   selector: 'app-root',
@@ -10,15 +11,24 @@ import { BreadcrumbComponent } from './shared/components/breadcrumbs/breadcrumbs
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Frontend';
   showNavbar = true;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private globalStateService: GlobalStateService) {
     // escuchar cambios de ruta
     this.router.events.subscribe(() => {
       this.updateNavbarVisibility();
     });
+  }
+
+  ngOnInit(): void {
+      const authUser = localStorage.getItem('authUser');
+      if (authUser) {
+        const userData = JSON.parse(authUser);
+        this.globalStateService.setCenterId(userData.centerId);
+        this.globalStateService.setUserId(userData.userId);
+      }
   }
 
   private updateNavbarVisibility() {
