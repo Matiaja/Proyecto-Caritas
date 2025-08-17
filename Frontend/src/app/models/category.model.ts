2127429dcef2215ef0f97ce7df39ac82a228b414
chipcasla <alejandroforesi15@gmail.com>
2025-08-17 20:08:04 -0300

    feat: enhance category detail component to display associated products in a table

--- Frontend/src/app/models/category.model.ts
+++ Frontend/src/app/models/category.model.ts
@@ -1,5 +1,8 @@
+import { Product } from "./product.model";
+
 export interface Category {
   id: number;
   name: string;
   description: string;
+  products?: Product[]; // Optional property to hold products associated with the category
 }
--- Frontend/src/app/pages/category/category-detail/category-detail.component.ts
+++ Frontend/src/app/pages/category/category-detail/category-detail.component.ts
@@ -3,11 +3,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Category } from '../../../models/category.model';
 import { CategoryService } from '../../../services/category/category.service';
 import { CommonModule, Location } from '@angular/common';
+import { UiTableComponent } from "../../../shared/components/ui-table/ui-table.component";
 
 @Component({
   selector: 'app-category-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UiTableComponent],
   templateUrl: './category-detail.component.html',
   styleUrl: './category-detail.component.css',
 })
@@ -17,6 +18,21 @@ export class CategoryDetailComponent implements OnInit {
     name: '',
     description: '',
   } as Category;
+
+  // Datos de la tabla
+  products: any[] = [];
+  displayedColumns = ['id', 'code', 'name'];
+  columnHeaders = {
+    id: 'ID',
+    code: 'Código',
+    name: 'Nombre'
+  };
+  title = 'Productos de la categoría';
+  mobileColumns = ['code', 'name'];
+  mobileHeaders = {
+    code: 'Código',
+    name: 'Nombre'
+  };
 
   constructor(
     private categoryService: CategoryService,
@@ -36,6 +52,7 @@ export class CategoryDetailComponent implements OnInit {
     this.categoryService.getCategory(categoryId).subscribe({
       next: (category: Category) => {
         this.category = category;
+        this.products = category.products || [];
       },
     });
   }
